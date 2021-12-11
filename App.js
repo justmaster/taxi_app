56da884b4ff4b5fd1fba83c553351b92241fc92d
justmaster <tornike-10@yandex.ru>
2021-12-11 22:55:58 +0400

    lmost finished only styles for sidebar left

--- App.js
+++ App.js
@@ -1,21 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
+import {gestureHandlerRootHOC} from 'react-native-gesture-handler';
+
+import 'react-native-reanimated'
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, Platform} from 'react-native';
+import { Provider } from "react-redux"
+import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
+import OptionsScreen from './components/drawercomponents/Options';
+import ProfileScreen from './components/drawercomponents/Profile';
+import { store } from './store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          
+          <KeyboardAvoidingView 
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={{ flex: 1}}
+            keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
+          >
+
+          <Drawer.Navigator initialRouteName='Home' >
+            <Drawer.Screen 
+              name="Home" 
+              component={HomeScreen}
+              options={{
+                headerShown: false
+              }}/>
+            <Drawer.Screen 
+              name="MapScreen" 
+              component={MapScreen}
+              options={{
+                headerShown: false,
+                drawerItemStyle: { height: 0 }
+                
+              }}/>
+              <Drawer.Screen 
+              name="Options" 
+              component={OptionsScreen}
+              options={{
+                headerShown: false,
+                
+              }}/>
+              <Drawer.Screen 
+              name="Profile" 
+              component={ProfileScreen}
+              options={{
+                headerShown: false,
+                
+              }}/>
+          </Drawer.Navigator>
+
+          </KeyboardAvoidingView>
+
+        </SafeAreaProvider>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
